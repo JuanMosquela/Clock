4bd1cfe8075c2ba0d37c84835ce148d821e735d2
Juan Mosquella <jmosquella11@gmail.com>
2022-04-26 16:33:32 -0300

    Entrega manipulando DOM para coder

--- app.js
+++ app.js
@@ -1,22 +1,20 @@
-
-
-
-
+const div = document.createElement('div');
+div.className = 'time';
+document.querySelector('.container-clock').appendChild(div);
 
 const showClock = () => {
     
     const time = new Date();
     const hours = time.getHours();
     const minutes = time.getMinutes();
-    const seconds = time.getSeconds();
-
-    document.getElementById('time').innerHTML = `${hours}:${minutes}:${seconds}`
-
-      
+    const seconds = time.getSeconds();    
+    
+    let timeNro = `${hours}:${minutes}:${seconds}`;
+    div.innerHTML = timeNro;    
+    
+        
 }
 
 showClock()
-
-
 
 setInterval(showClock, 1000)
